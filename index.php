6bdad0734ed1bc0687d1e2625523e597ab4f2a18
Elmoufid <elmoufid@MacBook-Airsoft.local>
2018-04-04 09:48:55 +0200

    index

--- index.php
+++ index.php
@@ -1,15 +1,32 @@
+<?php include('inc/pdo.php');?>
+<?php include('inc/function.php');  ?>
 
-<?php 
-include('inc/pdo.php');
+<?php include('inc/header.php'); ?>
 
-include('inc/header.php');
-
-
-
-
-
-include('inc/footer.php');
+<?php
+$sql = "SELECT * FROM movies_full WHERE `id` = ROUND( RAND() * 9 ) + 1 ";
+$query = $pdo->prepare($sql);
+// exécuté la requete
+$query ->execute();
+// Puis sous quel format on la veut
+$movies = $query->fetchAll();
 
 ?>
 
+<?php
+echo '<pre>';
+print_r($movies);
+echo '</pre>';
+ ?>
 
+<div id="movies">
+	<?php foreach ($movies as $movie) { ?>
+	<div class="movie">
+		<p class="title"> <?php echo $movie ['title'];?> </p>
+		<a href="detail.php?id=<?php echo $movie['id']; ?>">
+			<?php echo getImageMovie($movie['id'],$movie['title']);?>
+		</a>
+	</div>
+<?php } ?>
+</div>
+<?php include('inc/footer.php') ?>
