06eac2743f13e8f56098c84425d0cbf7a0cb3cc3
kanku kabasele <Kariza@MacBook-Pro-de-kanku.local>
2018-04-04 12:08:16 +0200

    kjhkjh

--- details.php
+++ details.php
@@ -1,22 +1,14 @@
-<!-- /////////////////  -  La page de détail
-//Outre l’affiche du film et son titre, toutes les données présentes en base de données sont affichées.
-
-///////////////////////////  PAGE DE DETAILS
-// Outre l’affichage du poster et de tous les détails du film, les éléments suivants doivent être présents :
-	// - Pretty Url
-		// - L’url de ces pages de détails devrait contenir la version normalisée du titre du film (slug). Et tant qu’à y être, 
-        faites tout votre possible pour référencer au mieux cette page par rapport au titre du film et de son année de réalisation. -->
-
 <?php
 include('inc/pdo.php');
 include('inc/function.php');
-// include('inc/index.php');
-
-
-include('inc/header.php'); 
-
 
 if(!empty($_GET['id']) && is_numeric($_GET['id'])) {
+    $sql = "SELECT * FROM movies_full";
+	$query = $pdo->prepare($sql);
+	// exécuté la requete
+	$query ->execute();
+	// Puis sous quel format on la veut
+	$movies = $query->fetchAll();
    $id = $_GET['id'];
     
 }else{
