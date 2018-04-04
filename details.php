8e9881b50b797378d41928e0a29aa8fc27ec8471
kanku kabasele <Kariza@MacBook-Pro-de-kanku.local>
2018-04-04 13:00:18 +0200

    13h

--- details.php
+++ details.php
@@ -26,28 +26,28 @@ $id = $_GET['id'];
 <?php 
 foreach  ($movies as $movie) { 
     if($movie['id'] == $id){ ?>
-            <div class="container">
-                <p class='title'>Titre du film: <?php echo $movie['title']; ?></p>
-                <p class='slug'>Slug: <?php echo $movie['slug']; ?></p>
-                <p class='year'>Année: <?php echo $movie['year']; ?></p>
-                <p class='genres'>Genres: <?php echo $movie['genres']; ?></p>
-                <p class='plot'>Pitch: <?php echo $movie['plot']; ?></p>
-                <p class='directors'>Réalisateur: <?php echo $movie['directors']; ?></p>
-                <p class='cast'>Casting: <?php echo $movie['cast']; ?></p>
-                <p class='writers'>Scénaristes: <?php echo $movie['writers']; ?></p>
-                <p class='runtime'>Durée du film: <?php echo $movie['runtime']; ?></p>
-                <p class='mpaa'>Motion Picture Association of America: <?php echo $movie['mpaa']; ?></p>
-                <p class='rating'>Note: <?php echo $movie['rating']; ?></p>
-                <p class='popularity'>Popularité: <?php echo $movie['popularity']; ?></p>
-                <p class='modified'>modifié le: <?php echo $movie['modified']; ?></p>
-                <p class='created'>créé le: <?php echo $movie['created']; ?></p>
-                <p class='poster_flag'>Affiche du film: <?php echo $movie['poster_flag']; ?></p>
+        <div class="container">
+            <p class='title'>Titre du film: <?php echo $movie['title']; ?></p>
+            <p class='slug'>Slug: <?php echo $movie['slug']; ?></p>
+            <p class='year'>Année: <?php echo $movie['year']; ?></p>
+            <p class='genres'>Genres: <?php echo $movie['genres']; ?></p>
+            <p class='plot'>Pitch: <?php echo $movie['plot']; ?></p>
+            <p class='directors'>Réalisateur: <?php echo $movie['directors']; ?></p>
+            <p class='cast'>Casting: <?php echo $movie['cast']; ?></p>
+            <p class='writers'>Scénaristes: <?php echo $movie['writers']; ?></p>
+            <p class='runtime'>Durée du film: <?php echo $movie['runtime']; ?></p>
+            <p class='mpaa'>Motion Picture Association of America: <?php echo $movie['mpaa']; ?></p>
+            <p class='rating'>Note: <?php echo $movie['rating']; ?></p>
+            <p class='popularity'>Popularité: <?php echo $movie['popularity']; ?></p>
+            <p class='modified'>modifié le: <?php echo $movie['modified']; ?></p>
+            <p class='created'>créé le: <?php echo $movie['created']; ?></p>
+            <p class='poster_flag'>Affiche du film: <?php echo $movie['poster_flag']; ?></p>
 
-                <a href="details.php?id=<?php echo $movie['id'];?>">
-                <img src="posters/<?php echo $movie['id'] ?>.jpg" alt=""></a>
-        
-        
-            </div>
+            <a href="details.php?id=<?php echo $movie['id'];?>">
+            <img src="posters/<?php echo $movie['id'] ?>.jpg" alt=""></a>
+    
+    
+        </div>
     <?php }
 } ?>
 
