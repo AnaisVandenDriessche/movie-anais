758f756e8b2e9a6db9cad04744fd5644c2336824
Elmoufid <elmoufid@MacBook-Airsoft.local>
2018-04-04 11:49:15 +0200

    11H49

--- index.php
+++ index.php
@@ -1,38 +1,41 @@
 <?php include('inc/pdo.php');?>
 <?php include('inc/function.php');  ?>
 
-<?php include('inc/header.php'); ?>
+
 
 <?php
-$sql = "SELECT * FROM movies_full";
+$sql = "SELECT * FROM movies_full WHERE RAND() "  ;
 $query = $pdo->prepare($sql);
 // exécuté la requete
 $query ->execute();
 // Puis sous quel format on la veut
 $movies = $query->fetchAll();
 
-?> 
+?>
 
 <!-- <?php
 // echo '<pre>';
 // print_r($movies);
 // echo '</pre>';
  ?> -->
+<?php include('inc/header.php'); ?>
 
+
+<!-- <form class="" action="index.html" method="post">
+
+</form> -->
 
 	<?php foreach ($movies as $movie) { ?>
+		<div class="container">
 
-	<div class="container">
-		<div class="row">
-			<div class="col-4">
-				
-				<a href="details.php?id=<?php echo $movie['id']; ?>"><img src="posters/<?php echo $movie['id'] ?>.jpg" alt=""></a>
-				<p class="title"> <?php echo $movie ['title'];?> </p>
-			</div>
+				<div class="col-6">
+					<a href="details.php?id=<?php echo $movie['id']; ?>"><img src="posters/<?php echo $movie['id'] ?>.jpg" alt=""></a>
+					<p class="title"> <?php echo $movie ['title'];?> </p>
+				</div>
+		
 		</div>
-	</div>
 	<?php }
 	?>
-
-
+	<!-- Bouton + de film  -->
+	<a href="index.php"> + de films !  </a>
 <?php include('inc/footer.php') ?>
